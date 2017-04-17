eca5413b2c5e1b6e62d255f5c656b9e4707dae89
Oded Lazar <odedlaz@gmail.com>
2017-04-17 20:48:49 +0300

    [lab3] Move headers to include directory :pray:

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -6,33 +6,31 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
-#include "Kmeans.h"
+
+#include "include/Kmeans.h"
 
 using std::vector;
 using std::string;
 
-void readInput(const string& filename, bool has_name, vector<Point>& points )
-{
+void readInput(const string &filename, bool has_name, vector<Point> &points) {
     std::ifstream file;
-    file.open( filename.c_str(), std::ifstream::in);
+    file.open(filename.c_str(), std::ifstream::in);
 
     string line;
     // read data from file
     size_t pointID = 0;
 
-    while (getline(file, line))
-    {
+    while (getline(file, line)) {
         vector<double> values;
         std::istringstream iss(line);
         string point_name;
         iss >> point_name;
-        while (iss.good())
-        {
+        while (iss.good()) {
             double dval(0.0);
             iss >> dval;
             values.push_back(dval);
         }
-        Point p( pointID, values, point_name);
+        Point p(pointID, values, point_name);
         points.push_back(p);
         pointID++;
     }
@@ -40,18 +38,17 @@ void readInput(const string& filename, bool has_name, vector<Point>& points )
     file.close();
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     unsigned K(0), max_iterations(0);
-    if (argc < 5 )
+    if (argc < 5)
         return 1;
 
-    K = (unsigned)atoi(argv[1]);
-    max_iterations = (unsigned)atoi(argv[2]);
+    K = (unsigned) atoi(argv[1]);
+    max_iterations = (unsigned) atoi(argv[2]);
 
-    if ( (!K) || (!max_iterations)) return 1;
+    if ((!K) || (!max_iterations)) return 1;
 
-    bool has_name = (bool)atoi(argv[3]);
+    bool has_name = (bool) atoi(argv[3]);
     string fileName(argv[4]);
 
     vector<Point> points;
