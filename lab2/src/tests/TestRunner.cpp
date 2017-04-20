995ccfecc48b21c5deea477d20209d9241204ecd
afikbar <afikbar@campus.technion.ac.il>
2017-04-20 12:06:25 +0300

    [lab2] add code documentation :memo:

--- lab2/src/tests/TestRunner.cpp
+++ lab2/src/tests/TestRunner.cpp
@@ -5,7 +5,10 @@
 using std::cout;
 using std::endl;
 
-
+/**
+ * Test Run
+ * handles test running for this lab
+ */
 void runTests() {
     cout << "Running all tests..." << endl;
     printTestResult(testCorrelationSymmetry(),
@@ -13,6 +16,12 @@ void runTests() {
 
 }
 
+/**
+ * Print Tests Result
+ * prints the result of specific test.
+ * @param result Type Boolean
+ * @param testName Type string
+ */
 void printTestResult(bool result, std::string testName) {
     std::string resultText = result ? "Success" : "Fail";
     printf("%s: %s!\n", testName.c_str(),
