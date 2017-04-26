b3bf54f71483ba14631979700b2dfe6a1547bc66
Afik Bar <afikbar@campus.technion.ac.il>
2017-04-26 15:42:00 +0300

    [lab5] implement BSS class :gift:

--- lab5/src/BSS.cpp
+++ lab5/src/BSS.cpp
@@ -1,1 +1,15 @@
 #include "include/BSS.h"
+
+double BSS::calculate() const {
+
+    Point overallCentroid(getOverallCenter());
+    std::vector<Cluster>::const_iterator iter;
+    double bss(0.0);
+    for (iter = _clusters.begin(); iter != _clusters.end(); ++iter) {
+        Point clusterCentroid = (*iter).getCenterPoint();
+        double dist = clusterCentroid.squaredDistance(overallCentroid);
+        bss += dist * (*iter).getSize();
+    }
+
+    return bss;
+}
