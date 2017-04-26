54a5d554e36711a14f9025715b6a3823bc848042
Afik Bar <afikbar@campus.technion.ac.il>
2017-04-26 10:54:07 +0300

    [lab5] lint code :beer:

--- lab5/src/TSS.cpp
+++ lab5/src/TSS.cpp
@@ -1,6 +1,2 @@
-//
-// Created by annabel@staff.technion.ac.il on 4/24/17.
-//
-
 #include "include/TSS.h"
 
--- lab5/src/include/Point.h
+++ lab5/src/include/Point.h
@@ -1,7 +1,3 @@
-//
-// Created by anna on 4/22/2017.
-//
-
 #ifndef LAB5_POINT_H
 #define LAB5_POINT_H
 
@@ -10,8 +6,7 @@
 #include <string>
 
 
-class Point
-{
+class Point {
 private:
     size_t _idPoint;
     std::vector<double> _values;
@@ -22,21 +17,27 @@ public:
     static const size_t _no_id;
 
 public:
-    Point(const std::vector<double>& values, const std::string& name=_no_name, size_t id_point=_no_id);
+    Point(const std::vector<double> &values, const std::string &name = _no_name, size_t id_point = _no_id);
+
     Point(size_t dimension);
+
     size_t getID() const { return _idPoint; }
-    const std::string& getName() const { return _name; }
-    double euclideanDistance( const Point &other_point ) const;
-    double squaredDistance( const Point &other_point ) const;
-    bool operator==(const Point &other_point ) const;
+
+    const std::string &getName() const { return _name; }
+
+    double euclideanDistance(const Point &other_point) const;
+
+    double squaredDistance(const Point &other_point) const;
+
+    bool operator==(const Point &other_point) const;
+
     void print() const;
+
     void operator/=(double denominator);
-    friend Point operator+(const Point& point1, const Point& point2);
+
+    friend Point operator+(const Point &point1, const Point &point2);
 
 };
-
-
-
 
 
 #endif //LAB5_POINT_H
