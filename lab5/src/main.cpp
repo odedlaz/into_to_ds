ca807b2832a51f47e773aa82e5c192bf862e9553
Afik Bar <afikbar@campus.technion.ac.il>
2017-04-26 15:42:00 +0300

    [lab5] implement BSS class :gift:

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -12,60 +12,54 @@ using std::string;
 using std::vector;
 using std::endl;
 
-void printSilhouetteCoeff(const vector<Cluster>& clusters, size_t dimension)
-{
-    SilhouetteCoeff coeff( clusters, dimension );
-    std::cout <<"Silhouette total: "<< coeff.calculate() << endl;
+void printSilhouetteCoeff(const vector<Cluster> &clusters, size_t dimension) {
+    SilhouetteCoeff coeff(clusters, dimension);
+    std::cout << "Silhouette total: " << coeff.calculate() << endl;
     for (std::vector<Cluster>::size_type i = 0; i != clusters.size(); i++) {
-        std::cout << "Silhouette Cluster " << i << " : " << coeff.calculate( clusters[i] ) << endl;
+        std::cout << "Silhouette Cluster " << i << " : " << coeff.calculate(clusters[i]) << endl;
     }
-    size_t clusterID=2;
-    for (size_t j=0; j< clusters[clusterID].getSize(); j++)
-    {
+    size_t clusterID = 2;
+    for (size_t j = 0; j < clusters[clusterID].getSize(); j++) {
         clusters[clusterID][j].print();
-        std::cout << "Silhouette point "<< coeff.calculate( clusters[clusterID][j] ) << endl;
+        std::cout << "Silhouette point " << coeff.calculate(clusters[clusterID][j]) << endl;
     }
 }
 
-void printWSS(const vector<Cluster>& clusters, size_t dimension)
-{
-    WSS wss( clusters, dimension );
+void printWSS(const vector<Cluster> &clusters, size_t dimension) {
+    WSS wss(clusters, dimension);
     std::cout << "WSS: " << wss.calculate() << endl;
     for (std::vector<Cluster>::size_type i = 0; i != clusters.size(); i++) {
-        std::cout << "Cluster " << i << " : " << wss.calculate( clusters[i] ) << endl;
+        std::cout << "Cluster " << i << " : " << wss.calculate(clusters[i]) << endl;
     }
 }
 
-void printBSS(const vector<Cluster>& clusters, size_t dimension)
-{
-//    BSS bss(clusters, dimension );
-//    std::cout << "BSS: " << bss.calculate() << endl;
+void printBSS(const vector<Cluster> &clusters, size_t dimension) {
+    BSS bss(clusters, dimension);
+    std::cout << "BSS: " << bss.calculate() << endl;
 }
 
 
-void printTSS(const vector<Cluster>& clusters, size_t dimension)
-{
+void printTSS(const vector<Cluster> &clusters, size_t dimension) {
 //    TSS tss(clusters, dimension );
 //    std::cout << "TSS: " << tss.calculate() << endl;
 }
 
-void printPurity(const vector<Cluster>& clusters, size_t dimension)
-{
-    Purity purity(clusters, dimension );
+void printPurity(const vector<Cluster> &clusters, size_t dimension) {
+    Purity purity(clusters, dimension);
     std::cout << "Purity: " << purity.calculate() << endl;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc!=2) {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         std::cerr << "Missing data file" << std::endl;
         return 1;
     }
-    ClusterReader reader( argv[1] );
-    if (!reader.loadClusters() ) return 1;
-    printSilhouetteCoeff( reader.getClusters(), reader.getPointsDimension() );
-    printWSS(reader.getClusters(), reader.getPointsDimension() );
-//    printBSS(reader.getClusters(), reader.getPointsDimension() );
+    ClusterReader reader(argv[1]);
+    if (!reader.loadClusters()) return 1;
+    printSilhouetteCoeff(reader.getClusters(), reader.getPointsDimension());
+    printWSS(reader.getClusters(), reader.getPointsDimension());
+    printBSS(reader.getClusters(), reader.getPointsDimension());
 //    printTSS(reader.getClusters(), reader.getPointsDimension() );
-    printPurity(reader.getClusters(), reader.getPointsDimension() );
+    printPurity(reader.getClusters(), reader.getPointsDimension());
     return 0;
 }
