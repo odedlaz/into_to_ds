d23f192c0d3d597031806ff04eb980e56d2596c9
Oded Lazar <odedlaz@gmail.com>
2017-04-26 11:16:43 +0300

    [lab5] implement wss :gift:

--- lab5/src/WSS.cpp
+++ lab5/src/WSS.cpp
@@ -1,1 +1,15 @@
 #include "include/WSS.h"
+
+double WSS::calculate(const Cluster &cluster) const {
+    return cluster.sumSquaresPointToCluster(cluster.getCenterPoint());
+}
+
+double WSS::calculate() const {
+    std::vector<Cluster>::const_iterator it;
+
+    double sumAll(0.0);
+    for (it = _clusters.begin(); it != _clusters.end(); ++ it) {
+       sumAll +=  calculate(*it);
+    }
+    return sumAll;
+}
