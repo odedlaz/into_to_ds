e732e75f0f1485f91c157a31d2140161081826e4
Oded Lazar <odedlaz@gmail.com>
2017-04-29 19:48:29 +0300

    [lab4] add comments to Point :memo:

--- lab4/src/include/Point.h
+++ lab4/src/include/Point.h
@@ -15,7 +15,7 @@ private:
 public:
     Point(size_t id_point, const std::vector<double> &values, const std::string &name = "");
 
-    size_t getID() const { return _idPoint; }
+    size_t getID() const;
 
     double euclideanDistance(const Point &other_point) const;
 
