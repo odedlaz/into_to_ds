69f843dad4cbeeefbda2d2b620f64ebfbeb30248
Oded Lazar <odedlaz@gmail.com>
2017-04-29 19:48:29 +0300

    [lab4] add comments to Point :memo:

--- lab4/src/Point.cpp
+++ lab4/src/Point.cpp
@@ -6,11 +6,33 @@ using std::string;
 using std::cout;
 using std::endl;
 
+/*
+ * Constructor
+ * constructs a Point
+ * @param id_point an id that will represent this point
+ * @param values a vector<double> that holds the point's coordinates
+ * @param name a name to represent this point
+ * @return a double representing the distance
+ */
 Point::Point(size_t id_point, const std::vector<double> &values, const std::string &name) : _idPoint(id_point),
                                                                                             _values(values),
                                                                                             _name(name) {}
 
+/*
+ * getID
+ * Get the ID of this point
+ * @return a double representing the distance
+ */
+size_t Point::getID() const {
+    return _idPoint;
+}
 
+/*
+ * euclideanDistance
+ * calculates the euclidean distance between this point and another
+ * @param other_point another Point& instance to claculate distance from
+ * @return a double representing the distance
+ */
 double Point::euclideanDistance(const Point &other_point) const {
     double sum = 0.0;
 
@@ -21,6 +43,10 @@ double Point::euclideanDistance(const Point &other_point) const {
     return sqrt(sum);
 }
 
+/*
+ * Print
+ * Print the point's name and coordinates
+ */
 void Point::print() const {
 
     for (vector<double>::size_type i = 0; i < _values.size(); i++)
