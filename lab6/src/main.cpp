106f3d867ac6cca924c07a36bd72241f241579af
Afik Bar <afikbar@campus.technion.ac.il>
2017-05-10 11:41:04 +0300

    [lab6] implement question 1 & 2 :gift:

--- lab6/src/main.cpp
+++ lab6/src/main.cpp
@@ -14,6 +14,21 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+void question1(vector<Point> &allData) {
+    KNN knn(5);
+    knn.train(allData);
+    knn.predict(*allData.begin());
+    cout << "first element Prediction class: " << (*allData.begin()).getPrediction() << endl;
+
+
+}
+
+void question2(vector<Point> &allData) {
+    KNN knn(5);
+    knn.train(allData);
+    Evaluation evaluation(knn);
+    cout << "Accuracy is: " << evaluation.crossValidation(allData, 10) << endl;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -22,9 +37,16 @@ int main(int argc, char *argv[]) {
     }
     string fileName(argv[1]);
     DataReader dr;
-    vector <Point> allData;
+    vector<Point> allData;
     dr.read(fileName, allData);
     cout << allData.size() << endl;
+//    size_t folds = 10;
+//    Evaluation evaluation = new Evaluation()
+//    double accuracy = Evaluation::crossValidation(allData, folds);
+//    cout << accuracy << endl;
+    question1(allData);
+    question2(allData);
 
     return 0;
 }
+
