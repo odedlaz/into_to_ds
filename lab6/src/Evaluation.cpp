111cdd7c6657b6a8489a9f5e1afcdc18993c762e
Afik Bar <afikbar@campus.technion.ac.il>
2017-05-16 23:46:17 +0300

    [lab6] implement questions 1-3 :gift:

--- lab6/src/Evaluation.cpp
+++ lab6/src/Evaluation.cpp
@@ -7,11 +7,11 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-void Evaluation::splitData(const std::vector <Point> &data,
+void Evaluation::splitData(const std::vector<Point> &data,
                            size_t testFoldStart,
                            size_t testFoldEnd,
-                           std::vector <Point> &trainingSet,
-                           std::vector <Point> &testingSet) {
+                           std::vector<Point> &trainingSet,
+                           std::vector<Point> &testingSet) {
     size_t dimension = data[0].getDimension();
     testingSet.resize(testFoldEnd - testFoldStart, Point(dimension));
     trainingSet.resize(data.size() - testingSet.size(), Point(dimension));
@@ -20,13 +20,13 @@ void Evaluation::splitData(const std::vector <Point> &data,
     std::copy(data.begin() + testFoldEnd, data.end(), trainingSet.begin() + testFoldStart);
 }
 
-void Evaluation::test(std::vector <Point> &testingSet) {
+void Evaluation::test(std::vector<Point> &testingSet) {
     for (std::vector<Point>::size_type sz = 0; sz < testingSet.size(); sz++) {
         _knn.predict(testingSet[sz]);
     }
 }
 
-double Evaluation::crossValidation(std::vector <Point> &data, size_t folds) {
+double Evaluation::crossValidation(std::vector<Point> &data, size_t folds) {
     if (data.size() == 0)
         return 0;
     std::random_shuffle(data.begin(), data.end());
@@ -35,8 +35,8 @@ double Evaluation::crossValidation(std::vector <Point> &data, size_t folds) {
     //we are handling all folds besides the last one
     for (size_t id = 0; id < folds; id++) {
         _knn.reset();
-        std::vector <Point> training;
-        std::vector <Point> testing;
+        std::vector<Point> training;
+        std::vector<Point> testing;
         if (id != (folds - 1))
             splitData(data, id * foldSize, id * foldSize + foldSize, training, testing);
         else
@@ -45,7 +45,9 @@ double Evaluation::crossValidation(std::vector <Point> &data, size_t folds) {
         if (!_knn.train(training))
             return 0;
         test(testing);
-        totalAccuracy += EvaluationMeasures::accuracy(testing);
+        double currTestAccuracy = EvaluationMeasures::accuracy(testing);
+//        cout << "Current testing fold Accuracy: " << currTestAccuracy << endl;
+        totalAccuracy += currTestAccuracy;
     }
     return totalAccuracy / (double) folds;
 }
--- lab6/src/main.cpp
+++ lab6/src/main.cpp
@@ -14,21 +14,57 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-void question1(vector<Point> &allData) {
+double runCrossValidation(vector<Point> &allData, int k, int folds) {
+    KNN knn(k);
+    if (knn.train(allData)) {
+        Evaluation evaluation(knn);
+        return evaluation.crossValidation(allData, folds);
+    }
+    return -1;
+}
+
+void trainKnn5(vector<Point> &allData) {
     KNN knn(5);
-    knn.train(allData);
-    knn.predict(*allData.begin());
-    cout << "first element Prediction class: " << (*allData.begin()).getPrediction() << endl;
+    if (knn.train(allData)) {
+        knn.predict(*allData.begin());
+        cout << "First element Prediction class: " << (*allData.begin()).getPrediction() << endl;
+    }
 
+}
 
+void evaluationTenFold(vector<Point> &allData) {
+    cout << "10-fold Cross Validation is: " << runCrossValidation(allData, 5, 10) << endl;
+    /*KNN knn(5);
+    if (knn.train(allData)) {
+        Evaluation evaluation(knn);
+        cout << "10-fold Cross Validation is: " << evaluation.crossValidation(allData, 10) << endl;
+    }*/
+}
+
+void question1(vector<Point> &allData) {
+    KNN knn(1);
+    if (knn.train(allData)) {
+        for (vector<Point>::iterator iter = allData.begin(); iter != allData.end(); iter++) {
+            knn.predict(*iter);
+        }
+        cout << "Accuracy for KNN1 is: " << EvaluationMeasures::accuracy(allData) << endl;
+    }
 }
 
 void question2(vector<Point> &allData) {
-    KNN knn(5);
-    knn.train(allData);
-    Evaluation evaluation(knn);
-    cout << "Accuracy is: " << evaluation.crossValidation(allData, 10) << endl;
+    double bestAccuracy = 0.0;
+    int bestK = 0;
+    for (int k = 1; k <= 30; k++) {
+        double currentAccuracy = runCrossValidation(allData, k, allData.size());
+        if (currentAccuracy > bestAccuracy) {
+            bestAccuracy = currentAccuracy;
+            bestK = k;
+        }
+        cout << "leave-one-out Cross Validation for: " << k << "-Neighbours is: " << currentAccuracy << endl;
+    }
+    cout << bestK << " is the best Classifier. Best Accuracy is: " << bestAccuracy << endl;
 }
+
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -39,9 +75,18 @@ int main(int argc, char *argv[]) {
     DataReader dr;
     vector<Point> allData;
     dr.read(fileName, allData);
-    cout << allData.size() << endl;
+    cout << "Data size: " << allData.size() << endl;
+    trainKnn5(allData);
+    evaluationTenFold(allData);
+    cout << "   Question 1:" << endl;
     question1(allData);
+    cout << "   Question 2:" << endl;
     question2(allData);
+    cout << "   Question 3:" << endl;
+    cout << "2-fold-cross-validation:" << runCrossValidation(allData, 8, 2) << endl;
+    cout << "10-fold-cross-validation:" << runCrossValidation(allData, 8, 10) << endl;
+    cout << "leave-one-out-cross-validation:" << runCrossValidation(allData, 8, allData.size()) << endl;
+
 
     return 0;
 }
