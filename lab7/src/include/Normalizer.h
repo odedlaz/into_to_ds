24082d9e69864c44abfadd0aef3e765f3dfd5b1f
Afik Bar <afikbar@Gmail.com>
2017-05-17 11:57:03 +0300

    [lab7] add Normalizer pure virtual func. :gift:

--- lab7/src/include/Normalizer.h
+++ lab7/src/include/Normalizer.h
@@ -5,4 +5,13 @@
 #include "Point.h"
 
 
+
+
+class Normalizer {
+public:
+    virtual bool init(const std::vector<Point> &allData) = 0;
+    virtual void normalize(const Point &oldPoint, Point &newPoint) = 0;
+};
+
+
 #endif //LAB7_NORMALIZER_H
