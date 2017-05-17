be1557e5ac7d5b8f1463c464a3be3d377983ca28
Afik Bar <afikbar@Gmail.com>
2017-05-17 11:57:36 +0300

    [lab7] add Point / operator :gift:

--- lab7/src/Point.cpp
+++ lab7/src/Point.cpp
@@ -11,7 +11,12 @@ Point Point::operator-(const Point &otherPoint) const {
 }
 
 Point Point::operator/(const Point &otherPoint) const {
-
+    Point outcome(getDimension());
+    for (size_t i = 0; i < getDimension(); i++) {
+        outcome[i] = _values[i] / otherPoint[i];
+    }
+    outcome._class = _class;
+    return outcome;
 }
 
 std::ostream &operator<<(std::ostream &os, const Point &point) {
