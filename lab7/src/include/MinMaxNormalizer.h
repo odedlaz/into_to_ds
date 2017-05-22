6eabf07f82b9dd27301c4d8cf71ab759e52d18fd
Afik Bar <afikbar@Gmail.com>
2017-05-22 16:28:09 +0300

    [lab7] implement MinMaxNormalization :gift:

--- lab7/src/include/MinMaxNormalizer.h
+++ lab7/src/include/MinMaxNormalizer.h
@@ -3,5 +3,30 @@
 
 #include "Normalizer.h"
 
+class MinMaxNormalizer : public Normalizer {
+private:
+    Point min;
+    Point max;
+    Point delta;
+public:
+    /**
+     * Receives the point's amount of dimensions initializing min, max and delta to same size.
+     * @param dimension size_t amount of dimensions
+     */
+    MinMaxNormalizer(size_t dimension) : min(dimension), max(dimension), delta(dimension) {}
+    /**
+     * initializing the min, max & delta with the given dataset
+     * @param points Vector<Points> that contains dataset
+     * @return boolean result if init was successful.
+     */
+    bool init(const std::vector<Point> &points);
+
+    /**
+     * normalize oldPoint to newPoint after init is successful.
+     * @param oldPoint Point to be normalized
+     * @param newPoint Point to hold the normalized value
+     */
+    void normalize(const Point &oldPoint, Point &newPoint);
+};
 
 #endif //LAB7_MINMAXNORMALIZER_H
