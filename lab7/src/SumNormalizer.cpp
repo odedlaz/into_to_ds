268f6d61ea00ecdb2d64b5ad6409090c2fcc5123
Oded Lazar <odedlaz@gmail.com>
2017-05-22 18:20:20 +0300

    [lab7] minor performance improvements :pray:

--- lab7/src/SumNormalizer.cpp
+++ lab7/src/SumNormalizer.cpp
@@ -1,16 +1,21 @@
 #include "include/SumNormalizer.h"
 
 bool SumNormalizer::init(const std::vector<Point> &points) {
-    if (points.empty())
+    if (points.empty()) {
         return false;
-    for (std::vector<Point>::const_iterator iter = points.begin(); iter != points.end(); iter++){
-        for (size_t dim = 0; dim < points[0].getDimension(); dim++) {
+    }
+
+    std::vector<Point>::const_iterator iter;
+    size_t dimension = points[0].getDimension();
+    for (iter = points.begin(); iter != points.end(); iter++) {
+        for (size_t dim = 0; dim < dimension; dim++) {
             totalSum[dim] += (*iter)[dim];
         }
     }
+
     return true;
 }
 
 void SumNormalizer::normalize(const Point &oldPoint, Point &newPoint) {
-    newPoint = oldPoint/totalSum;
+    newPoint = oldPoint / totalSum;
 }
