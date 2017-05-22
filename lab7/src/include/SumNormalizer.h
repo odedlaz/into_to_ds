b6f2a52c063fd14ee8db9c621569ce0114300daa
Afik Bar <afikbar@Gmail.com>
2017-05-22 16:28:47 +0300

    [lab7] implement SumNormalization :gift:

--- lab7/src/include/SumNormalizer.h
+++ lab7/src/include/SumNormalizer.h
@@ -3,5 +3,30 @@
 
 #include "Normalizer.h"
 
+class SumNormalizer : public Normalizer {
+private:
+    Point totalSum;
+public:
+    /**
+     * Receives the point's amount of dimensions initializing totalSum to same size.
+     * @param dimension size_t amount of dimensions
+     */
+    SumNormalizer(size_t dimension) : totalSum(dimension) {}
+    /**
+     * initializing the totalSum with the given dataset
+     * @param points Vector<Points> that contains dataset
+     * @return boolean result if init was successful.
+     */
+    bool init(const std::vector<Point> &points);
+
+    /**
+     * normalize oldPoint to newPoint after init is successful.
+     * @param oldPoint Point to be normalized
+     * @param newPoint Point to hold the normalized value
+     */
+    void normalize(const Point &oldPoint, Point &newPoint);
+
+};
+
 
 #endif //LAB7_SUMNORMALIZER_H
