c5daea65c9b57a1c3a8ed3a7022468d4491f711e
Oded Lazar <odedlaz@gmail.com>
2017-05-22 18:20:20 +0300

    [lab7] minor performance improvements :pray:

--- lab7/src/MinMaxNormalizer.cpp
+++ lab7/src/MinMaxNormalizer.cpp
@@ -1,20 +1,26 @@
-#include <limits>
 #include "include/MinMaxNormalizer.h"
+#include <limits>
 
 bool MinMaxNormalizer::init(const std::vector<Point> &points) {
-    if (points.empty())
+    if (points.empty()) {
         return false;
+    }
     // initializing min and max to corresponding limit values.
-    for (size_t dim = 0; dim < min.getDimension(); dim++) {
+    size_t dimension = min.getDimension();
+    for (size_t dim = 0; dim < dimension; dim++) {
         min[dim] = std::numeric_limits<double>::max();
         max[dim] = std::numeric_limits<double>::min();
     }
-    for (std::vector<Point>::const_iterator iter = points.begin(); iter != points.end(); iter++) {
-        for (size_t dim = 0; dim < points[0].getDimension(); dim++) {
-            min[dim] = (*iter)[dim] > min[dim] ? min[dim] : (*iter)[dim];
-            max[dim] = (*iter)[dim] < max[dim] ? max[dim] : (*iter)[dim];
+
+    std::vector<Point>::const_iterator iter;
+    for (iter = points.begin(); iter != points.end(); iter++) {
+        for (size_t dim = 0; dim < dimension; dim++) {
+            double coordinate = (*iter)[dim];
+            min[dim] = coordinate > min[dim] ? min[dim] : coordinate;
+            max[dim] = coordinate < max[dim] ? max[dim] : coordinate;
         }
     }
+
     delta = max - min; // to avoid calculation of this value each normalization.
     return true;
 }
