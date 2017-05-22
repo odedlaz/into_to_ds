79c8576778e26b15c10e802eed1c5541ef7e1544
Afik Bar <afikbar@Gmail.com>
2017-05-22 16:28:47 +0300

    [lab7] implement SumNormalization :gift:

--- lab7/src/SumNormalizer.cpp
+++ lab7/src/SumNormalizer.cpp
@@ -1,2 +1,16 @@
 #include "include/SumNormalizer.h"
 
+bool SumNormalizer::init(const std::vector<Point> &points) {
+    if (points.empty())
+        return false;
+    for (std::vector<Point>::const_iterator iter = points.begin(); iter != points.end(); iter++){
+        for (size_t dim = 0; dim < points[0].getDimension(); dim++) {
+            totalSum[dim] += (*iter)[dim];
+        }
+    }
+    return true;
+}
+
+void SumNormalizer::normalize(const Point &oldPoint, Point &newPoint) {
+    newPoint = oldPoint/totalSum;
+}
