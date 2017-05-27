b21f1cc81990c4d746ebfe3216f1e2500febc073
Oded Lazar <odedlaz@gmail.com>
2017-05-27 18:25:08 +0300

    [lab7] add more comments :memo

--- lab7/src/include/Normalizer.h
+++ lab7/src/include/Normalizer.h
@@ -9,7 +9,18 @@
 
 class Normalizer {
 public:
+    /**
+     * initializing the min, max & delta with the given dataset
+     * @param points Vector<Points> that contains dataset
+     * @return boolean result if init was successful.
+     */
     virtual bool init(const std::vector<Point> &allData) = 0;
+
+    /**
+     * normalize oldPoint to newPoint after init is successful.
+     * @param oldPoint Point to be normalized
+     * @param newPoint Point to hold the normalized value
+     */
     virtual void normalize(const Point &oldPoint, Point &newPoint) = 0;
 };
 
