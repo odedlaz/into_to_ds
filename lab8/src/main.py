d37d884398e35e2886fac08d1caf12e3b93ceeb3
Afik Bar <afikbar@Gmail.com>
2017-05-29 08:37:53 +0300

    [lab8] fix eculidian & implement predict :gift:

--- lab8/src/main.py
+++ lab8/src/main.py
@@ -1,6 +1,7 @@
 import file_reader
 import rocchio_classifier
 
+
 def calc_accuracy(test_set, class_centroids):
     correct = 0.0
     total = len(test_set.keys())
@@ -10,15 +11,14 @@ def calc_accuracy(test_set, class_centroids):
         print(real, predicted)
         if real == predicted:
             correct += 1.0
-    return (correct/total)
     return (correct / total)
 
 
 if __name__ == '__main__':
-    file_name = "./dataset/amazon_cells_labelled_full.txt"
+    file_name = "../dataset/amazon_cells_labelled_full.txt"
     word_bank = file_reader.create_words_bank(file_name)
     full_set = file_reader.build_set(file_name, word_bank)
-    train_set = file_reader.build_set("./dataset/amazon_cells_labelled_train.txt", word_bank)
-    test_set = file_reader.build_set("./dataset/amazon_cells_labelled_test.txt", word_bank)
+    train_set = file_reader.build_set("../dataset/amazon_cells_labelled_train.txt", word_bank)
+    test_set = file_reader.build_set("../dataset/amazon_cells_labelled_test.txt", word_bank)
     class_centroids = rocchio_classifier.training(train_set)
     print(calc_accuracy(test_set, class_centroids))
--- lab8/src/rocchio_classifier.py
+++ lab8/src/rocchio_classifier.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 
 def training(training_set):
@@ -8,19 +9,27 @@ def training(training_set):
         if doc_class not in class_centroids.keys():
             class_centroids[doc_class] = training_set[doc][0:-1]
         else:
-            class_centroids[doc_class] = [class_centroids[doc_class][i] + training_set[doc][0:-1][i] for i in range(len(training_set[doc]) - 1)]
             class_centroids[doc_class] = [class_centroids[doc_class][i] + training_set[doc][0:-1][i] for i in
                                           range(len(training_set[doc]) - 1)]
     for c in class_centroids.keys():
         class_size = len(class_centroids[c])
         for i in range(class_size):
             class_centroids[c][i] /= float(class_size)
+    print "class 1 centroid {}".format(class_centroids['1'])
+    print "class 0 centroid {}".format(class_centroids['0'])
     return class_centroids
 
 
 def predict(class_centroids, doc_vec):
-    return None
+    min_dist = sys.float_info.max
+    minCluster = None
+    for c in class_centroids.keys():
+        dist = euclidean_dist(class_centroids[c], doc_vec)
+        if (dist < min_dist):
+            min_dist = dist
+            minCluster = c
+    return minCluster
 
 
 def euclidean_dist(vec1, vec2):
-    return math.sqrt(map(lambda x: math.pow(x[0] - x[1], 2), zip(vec1, vec2)))
+    return math.sqrt(sum(map(lambda x: math.pow(x[0] - x[1], 2), zip(vec1, vec2))))
