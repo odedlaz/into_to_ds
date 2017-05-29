8ebb19f84eb2a8cb239a5d5c5475e8f710410a84
Oded Lazar <odedlaz@gmail.com>
2017-05-29 11:12:38 +0300

    [lab8] performance imporvement :beer:

--- lab8/src/rocchio_classifier.py
+++ lab8/src/rocchio_classifier.py
@@ -1,5 +1,5 @@
 import math
-import sys
+from itertools import starmap
 
 
 def training(training_set):
@@ -15,21 +15,28 @@ def training(training_set):
         class_size = len(class_centroids[c])
         for i in range(class_size):
             class_centroids[c][i] /= float(class_size)
+
     print "class 1 centroid {}".format(class_centroids['1'])
     print "class 0 centroid {}".format(class_centroids['0'])
+
     return class_centroids
 
 
 def predict(class_centroids, doc_vec):
-    min_dist = sys.float_info.max
-    minCluster = None
-    for c in class_centroids.keys():
-        dist = euclidean_dist(class_centroids[c], doc_vec)
+    items = class_centroids.iteritems()
+
+    min_cluster, item_vec = next(items)
+    min_dist = euclidean_dist(item_vec, doc_vec)
+
+    for item_class, item_vec in items:
+        dist = euclidean_dist(item_vec, doc_vec)
+
         if (dist < min_dist):
             min_dist = dist
-            minCluster = c
-    return minCluster
+            min_cluster = item_class
+
+    return min_cluster
 
 
 def euclidean_dist(vec1, vec2):
-    return math.sqrt(sum(map(lambda x: math.pow(x[0] - x[1], 2), zip(vec1, vec2))))
+    return math.sqrt(sum(starmap(lambda x, y: math.pow(x - y, 2), zip(vec1, vec2))))
