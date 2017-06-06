c92fdcb3ed00ad36aa8b32546052c6f0a91339b2
Oded Lazar <odedlaz@gmail.com>
2017-06-06 13:25:51 +0300

    [hw2] implement print results

--- hw2/src/Classifier.cpp
+++ hw2/src/Classifier.cpp
@@ -5,6 +5,7 @@
 #include "include/Classifier.h"
 #include "include/Point.h"
 #include <iostream>
+#include <cstdio>
 
 using std::cout;
 using std::endl;
@@ -12,7 +13,7 @@ using std::endl;
 double Classifier::accuracy(const std::vector<Point> &subset) const {
     double correct(0.0);
     for (size_t i = 0; i < subset.size(); i++) {
-        const Point & point = subset[i];
+        const Point &point = subset[i];
         if (point.getClass() == point.getPrediction())
             correct++;
     }
@@ -20,5 +21,8 @@ double Classifier::accuracy(const std::vector<Point> &subset) const {
 }
 
 void Classifier::printResults(const std::vector<Point> &subset) const {
-
+    for (int i = 0; i < subset.size(); i++) {
+        const Point & point = subset[i];
+        printf("%d,%s,%s\n", i, point.getPrediction().c_str(), point.getClass().c_str());
+    }
 }
