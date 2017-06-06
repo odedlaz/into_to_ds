7a91835f8afda4bb3ffbb868a7dca686f7f2a769
Oded Lazar <odedlaz@gmail.com>
2017-06-06 13:24:23 +0300

    [hw2] Implement manhattan distance

--- hw2/src/ManhattanDistance.cpp
+++ hw2/src/ManhattanDistance.cpp
@@ -3,7 +3,17 @@
 //
 
 #include "include/ManhattanDistance.h"
+#include <math.h>
 
 double ManhattanDistance::calculate(const Point &p1, const Point &p2) const {
-    return 0;
+    if (p1.getDimension() != p2.getDimension()) {
+        return 0;
+    }
+
+    double sum = 0.0;
+    for (size_t i = 0; i < p1.getDimension(); i++) {
+        sum += abs(p1[i] - p2[i]);
+    }
+    return sqrt(sum);
 }
+
