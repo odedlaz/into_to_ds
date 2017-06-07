3ff93bd40338a0dee9419978a9e222517a294b30
Oded Lazar <odedlaz@gmail.com>
2017-06-07 18:40:19 +0300

    [lab9] implement clean function

--- lab9/src/file_reader.py
+++ lab9/src/file_reader.py
@@ -1,49 +1,86 @@
-import math
-# from typing import Dict
+import re
 
-class File_Reader:
-    def __init__(self, input_file, vector_type='boolean'):
+BOOLEAN_TYPE = 'boolean'
+TF_TYPE = 'tf'
+TFIDF_TYPE = 'tfidf'
+
+
+punctuation_marks = {".", "?", "!",
+                     ";", ":", "-",
+                     "[", "]",
+                     "(", ")",
+                     "{", "}",
+                     "'", '"', "*"}
+
+
+class FileReader:
+    def __init__(self, input_file, words_filter=[], vector_type=BOOLEAN_TYPE):
+
+        if vector_type not in (BOOLEAN_TYPE, TF_TYPE, TFIDF_TYPE):
+            raise ValueError("vector type is not supported")
+
         self.file = input_file
-        self.vector_type = vector_type
-        self.words = {}  # type: Dict[str, int]
+        self._vector_type = vector_type
+        self._words_filter = words_filter
         self.df = {}  # type: Dict[str, int]
-        self.create_words_bank()
-        if self.vector_type == 'boolean':
-            self.build_set = self.build_set_boolean
-        if self.vector_type == 'tf':
-            self.build_set = self.build_set_tf
-        if self.vector_type == 'tfidf':
-            self.build_set = self.build_set_tfidf
 
-    @property
-    def vector_type(self):
-        return self.vector_type
+        words_pattern = "|".join([" %s " % re.escape(x)
+                                  for x in words_filter])
+        puncs_pattern = "[%s]" % re.escape("".join(punctuation_marks))
 
-    @vector_type.setter
-    def vector_type(self, value):
-        if value in ('boolean', 'tf', 'tfidf'):
-            self.vector_type = value
+        self.clean_pattern = re.compile("%s|%s" % (puncs_pattern, words_pattern),
+                                        re.IGNORECASE)
+
+        self.words = self.create_words_bank()
+        print(self.clean_pattern.pattern)
+
+    def _get_clean_words(self, line):
+        line = self.clean_pattern.sub(lambda x: "" if x.group()
+                                      in punctuation_marks
+                                      else " ", line.strip())
+        return line.split(" ")
+
+    def build_set(self, *args, **kwargs):
+        if self._vector_type == BOOLEAN_TYPE:
+            return self._build_set_boolean(*args, **kwargs)
+
+        if self._vector_type == TF_TYPE:
+            return self._build_set_tf(*args, **kwargs)
+
+        if self._vector_type == TFIDF_TYPE:
+            return self._build_set_tfidf(*args, **kwargs)
 
     def create_words_bank(self):
-        index = 1 # starting with 1 because 0 cant serve as a dictionary key
-        with open(self.file, 'r') as reader: # open the file "file"
-            for line in reader: # for each line in file
-                for word in line.split("\t")[0].split(): # for each word in the line
-                    if word not in self.words.keys(): # if the word doesnt already exists in the words dictionary
-                        self.words[word] = index # add it
-                        index += 1
+        # starting with 1 because 0 cant serve as a dictionary key
+        index = 1
 
-    def build_set_boolean(self, file_to_vector):
+        words = {}
+        with open(self.file, 'r') as f:
+            for raw_line in f:
+                line = raw_line[:raw_line.index("\t")]
+                for word in self._get_clean_words(line):
+                    # if the word already exists -> discard it
+                    if word in words:
+                        continue
+
+                    # only increase the index if the word exists
+                    # TODO: understand why?!
+                    words[word], index = index, index + 1
+
+        return words
+
+    def _build_set_boolean(self, file_to_vector):
         doc_set = {}
-        index = 0
-        with open(file_to_vector, 'r') as reader:
-            for line in reader:
-                vec = len(self.words)*[0,]
-                for word in line.split("\t")[0].split():
-                    vec[self.words[word]-1] = 1
-                doc_class = line.split("\t")[1].rstrip()
-                vec.append(doc_class)
-                doc_set['doc'+str(index)] = vec
-                index += 1
-        return doc_set
 
+        with open(file_to_vector, 'r') as f:
+            for index, line in enumerate(f):
+                vec = len(self.words) * [0]
+
+                raw_line, raw_doc_class = line.split("\t")
+                for word in self._get_clean_words(raw_line):
+                    vec[self.words[word] - 1] = 1
+
+                vec.append(raw_doc_class.rstrip())
+                doc_set['doc' + str(index)] = vec
+
+        return doc_set
