a84e98e606f8bf98c473fe8ba18a1dcbad22856e
Oded Lazar <odedlaz@gmail.com>
2017-07-01 16:12:48 +0300

    [hw3] refactor Reviewer to have real parameters :bug:

--- hw3/src/reviewer.py
+++ hw3/src/reviewer.py
@@ -5,14 +5,35 @@ from utils import ProgressBar
 
 
 class Reviewer(object):
-    def __init__(self, argument_parser):
-        self._parser = argument_parser
+
+    @classmethod
+    def from_argparse(cls, parser):
+            return cls(output_per_dir=parser.generate_per_dir,
+                       output_in_svm_light=parser.output_in_svm_light,
+                       output_in_tfidf=parser.generate_in_tfidf,
+                       words_filter=parser.stopwords,
+                       *parser.dirs)
+
+    def __init__(self, *dirs,
+                 words_filter=None,
+                 output_per_dir=False,
+                 output_in_svm_light=False,
+                 output_in_tfidf=False):
+
+        self._dirs = dirs
+
+        self._words_filter = words_filter or []
+
+        self._output_per_dir = output_per_dir
+        self._output_in_svm_light = output_in_svm_light
+        self._output_in_tfdidf = output_in_tfidf
+
         self._flags = self._get_flags_text()
 
     def _get_flags_text(self):
-        s = 'S' if self._parser.generate_per_dir else 's'
-        l = 'L' if self._parser.output_in_svm_light else 'l'
-        t = 'T' if self._parser.generate_in_tfidf else 't'
+        s = 'S' if self._output_per_dir else 's'
+        l = 'L' if self._output_in_svm_light else 'l'
+        t = 'T' if self._output_in_tfdidf else 't'
         return s + l + t
 
     def _get_filename(self, dir):
@@ -29,7 +50,7 @@ class Reviewer(object):
     def _write(self, filename, review_parser, reviews):
         # get the format of the review
         review_format = Review.RAW
-        if self._parser.output_in_svm_light:
+        if self._output_in_svm_light:
             review_format = Review.SVM
 
         # update the bag of words in accordance to the type of the review
@@ -47,9 +68,9 @@ class Reviewer(object):
                     f.write(line + '\n')
 
     def _write_per_dir(self):
-        for dir in map(os.path.abspath, self._parser.dirs):
+        for dir in map(os.path.abspath, self._dirs):
 
-            review_parser = ReviewParser(self._parser.stopwords)
+            review_parser = ReviewParser(self._words_filter)
             numoffiles = len(os.listdir(dir))
             filename = self._get_filename(dir)
 
@@ -62,26 +83,21 @@ class Reviewer(object):
 
             assert numoffiles == len(reviews)
 
-            self._write(filename,
-                        review_parser,
-                        reviews)
+            self._write(filename, review_parser, reviews)
 
     def _write_single_file(self):
-        review_parser = ReviewParser(self._parser.stopwords)
+        review_parser = ReviewParser(self._words_filter)
         filename = self._get_filename("dataset")
-        numoffiles = sum([len(os.listdir(x)) for x in
-                          self._parser.dirs])
+        numoffiles = sum(len(os.listdir(x)) for x in self._dirs)
 
         reviews = []
         prefix = "crunching reviews for '{}'".format(filename)
         with ProgressBar(prefix, numoffiles) as pb:
-            for dir in map(os.path.abspath, self._parser.dirs):
+            for dir in map(os.path.abspath, self._dirs):
                 for review in review_parser.parse_dir(dir):
                     pb.report()
                     reviews.append(review)
 
         assert numoffiles == len(reviews)
 
-        self._write(filename,
-                    review_parser,
-                    reviews)
+        self._write(filename, review_parser, reviews)
