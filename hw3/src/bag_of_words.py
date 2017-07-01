fb22a6102005517b5d98e08a9ee098bdba5bd0a4
Oded Lazar <odedlaz@gmail.com>
2017-07-01 11:47:27 +0300

    [hw3] move write logic to own class :gift:

--- hw3/src/bag_of_words.py
+++ hw3/src/bag_of_words.py
@@ -3,8 +3,7 @@ import os
 import sys
 
 import argparse_actions as actions
-from parser import Review, ReviewParser
-from utils import ProgressBar
+from reviewer import Reviewer
 
 
 def parse_arguments(args=None):
@@ -47,86 +46,7 @@ def parse_arguments(args=None):
     return parser.parse_args(args or sys.argv[1:])
 
 
-def get_flags_text(parser):
-    s = 'S' if parser.generate_per_dir else 's'
-    l = 'L' if parser.output_in_svm_light else 'l'
-    t = 'T' if parser.generate_in_tfidf else 't'
-    return s + l + t
-
-
-def get_results_filename(flags, dir, ext=""):
-    last_dirs = dir.split(os.sep)[-2:]
-    return "{}_{}.txt".format("_".join(last_dirs), flags)
-
-
-def write_reviews_to_disk(filename, review_parser, reviews):
-    # get the format of the review
-    review_format = Review.RAW
-    if argument_parser.output_in_svm_light:
-        review_format = Review.SVM
-
-    # update the bag of words in accordance to the type of the review
-    bag = review_parser.bag_of_words
-    if review_format == Review.SVM:
-        bag = {w: idx + 1 for idx, w
-               in enumerate(sorted(bag))}
-
-    prefix = "writing reviews to disk"
-    with ProgressBar(prefix, len(reviews)) as pb:
-        with open(filename, 'w') as f:
-            for review in reviews:
-                pb.report()
-                line = review.format(bag, review_format)
-                f.write(line + '\n')
-
-
-def generate_output_file_per_directory(argument_parser):
-    for dir in map(os.path.abspath, argument_parser.dirs):
-
-        review_parser = ReviewParser(argument_parser.stopwords)
-        numoffiles = len(os.listdir(dir))
-        filename = get_results_filename(flags, dir)
-
-        reviews = []
-        prefix = "crunching reviews for '{}'".format(filename)
-        with ProgressBar(prefix, numoffiles) as pb:
-            for review in review_parser.parse_dir(dir):
-                pb.report()
-                reviews.append(review)
-
-        assert numoffiles == len(reviews)
-
-        write_reviews_to_disk(filename,
-                              review_parser,
-                              reviews)
-
-
-def generate_single_output_file(argument_parser):
-    review_parser = ReviewParser(argument_parser.stopwords)
-    filename = get_results_filename(flags, "dataset")
-    numoffiles = sum([len(os.listdir(x)) for x in
-                      argument_parser.dirs])
-
-    reviews = []
-    prefix = "crunching reviews for '{}'".format(filename)
-    with ProgressBar(prefix, numoffiles) as pb:
-        for dir in map(os.path.abspath, argument_parser.dirs):
-            for review in review_parser.parse_dir(dir):
-                pb.report()
-                reviews.append(review)
-
-    assert numoffiles == len(reviews)
-
-    write_reviews_to_disk(filename,
-                          review_parser,
-                          reviews)
-
-
 if __name__ == "__main__":
     argument_parser = parse_arguments()
-    flags = get_flags_text(argument_parser)
-
-    if argument_parser.generate_per_dir:
-        generate_output_file_per_directory(argument_parser)
-    else:
-        generate_single_output_file(argument_parser)
+    reviewer = Reviewer(argument_parser)
+    reviewer.write(per_dir=argument_parser.generate_per_dir)
