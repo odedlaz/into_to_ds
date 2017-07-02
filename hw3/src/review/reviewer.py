75866bf1681f42473d479632408befef748ac8ea
Oded Lazar <odedlaz@gmail.com>
2017-07-02 21:48:58 +0300

    [hw3] add a lot of documentation and minor fixes :beer: :gift: :memo:

--- hw3/src/review/reviewer.py
+++ hw3/src/review/reviewer.py
@@ -11,17 +11,31 @@ class Reviewer(object):
 
     @classmethod
     def from_argparse(cls, parser):
-            return cls(output_per_dir=parser.generate_per_dir,
-                       output_in_svm_light=parser.output_in_svm_light,
-                       output_in_tfidf=parser.generate_in_tfidf,
-                       words_filter=parser.stopwords,
-                       *parser.dirs)
+        """
+        a constructor that extracts relevant args from an argparse parser
+        """
+        return cls(output_per_dir=parser.generate_per_dir,
+                   output_in_svm_light=parser.output_in_svm_light,
+                   output_in_tfidf=parser.generate_in_tfidf,
+                   words_filter=parser.stopwords,
+                   *parser.dirs)
 
     def __init__(self, *dirs,
                  words_filter=None,
                  output_per_dir=False,
                  output_in_svm_light=False,
                  output_in_tfidf=False):
+        """
+        Reviewer is in charge of the whole 'reviewing' process
+        it does everything needed to parse files, and dump the processed
+        outputs to disk
+
+        :param dirs: a list of directories to go through
+        :param words_filter: a list of words to discard when extracting words
+        :param output_per_dir: indicates if the output should be per dir or not
+        :param output_in_svm_light: indicate if output should be in svm or not
+        :param output_in_tfidf: indicate if output should be a sum of tfidf
+        """
 
         self._dirs = dirs
 
@@ -31,26 +45,40 @@ class Reviewer(object):
         self._output_in_svm_light = output_in_svm_light
         self._output_in_tfdidf = output_in_tfidf
 
-        self._flags = self._get_flags_text()
-
-    def _get_flags_text(self):
-        s = 'S' if self._output_per_dir else 's'
-        l = 'L' if self._output_in_svm_light else 'l'
-        t = 'T' if self._output_in_tfdidf else 't'
+    @staticmethod
+    def _get_flags_text(out_per_dir, svm_light_format, out_in_tfidf):
+        """
+        get flags text in 'slt' format
+        """
+        s = 'S' if out_per_dir else 's'
+        l = 'L' if svm_light_format else 'l'
+        t = 'T' if out_in_tfidf else 't'
         return s + l + t
 
-    def _get_filename(self, dir):
+    @staticmethod
+    def _get_filename(dir, flags):
+        """
+        create the filename of directory with the run flags
+        """
         last_dirs = dir.split(os.sep)[-2:]
-        return "{}_{}.txt".format("_".join(last_dirs),
-                                  self._flags)
+        return "{}_{}.txt".format("_".join(last_dirs), flags)
 
     def write(self):
+        """
+        perform all the logic - crunch data and write to disk.
+        """
         if self._write_per_dir:
             return self._write_per_dir()
 
         return self._write_single_file()
 
     def _write(self, filename, bag_of_words, reviews):
+        """
+        write reviews to disk
+        :param filename: the filename to write the output to
+        :param bag_of_words: an instance of BagOfWordsParser
+        :param reviews: all the reviews to write to disk
+        """
 
         # get the format of the review
         review_format = Review.RAW
@@ -68,12 +96,20 @@ class Reviewer(object):
                     f.write(line + '\n')
 
     def _write_per_dir(self):
+        """
+        write outputs of each directory to a different file
+        """
+
         bag_of_words = BagOfWordsParser(self._words_filter)
         reviews_per_dir = defaultdict(list)
 
+        flags = Reviewer._get_flags_text(self._output_per_dir,
+                                         self._output_in_svm_light,
+                                         self._output_in_tfdidf)
+
         for dir in map(os.path.abspath, self._dirs):
             numoffiles = len(os.listdir(dir))
-            filename = self._get_filename(dir)
+            filename = Reviewer._get_filename(dir, flags)
 
             reviews = reviews_per_dir[filename]
             prefix = "crunching reviews for '{}'".format(filename)
@@ -91,6 +127,9 @@ class Reviewer(object):
             self._write(filename, bag_of_words, reviews)
 
     def _write_single_file(self):
+        """
+        write outputs of all directory to one file
+        """
         bag_of_words = BagOfWordsParser(self._words_filter)
         filename = self._get_filename("dataset")
         numoffiles = sum(len(os.listdir(x)) for x in self._dirs)
--- hw3/src/utils/utils.py
+++ hw3/src/utils/utils.py
@@ -2,14 +2,24 @@ import sys
 
 
 class ProgressBar(object):
-
     def __init__(self, prefix, numofitems, file=sys.stdout):
+        """
+        ProgressBar write the progress of an operation to a file
+
+        :param prefix: a message to prefix when reporting percentage
+        :param numofitems: the num of items (used to calculate the percentage)
+        :param file: the file descriptor to write report string into.
+                     defaults to sys.stdout.
+        """
         self._numofitems = numofitems
         self._counter = 0
         self._file = file
         self._report_fmt = prefix + "... {0:.1f}%"
 
     def report(self):
+        """
+        report progress
+        """
         self._counter += 1
         progress = self._counter * 100.0 / self._numofitems
 
